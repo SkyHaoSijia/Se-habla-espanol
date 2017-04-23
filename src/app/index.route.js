2ea10d4ac31d74c5a0ae4ab0c366c67c32e5df9d
Sky <906564711@qq.com>
2017-04-23 18:47:31 +0800

    补提交

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -12,6 +12,31 @@
         controller: 'MainController',
         controllerAs: 'main'
       })
+      .when('/my_course',{
+        templateUrl:'app/main/my_course/my_course.html',
+        controller:'MyCourseCtrl',
+        controllerAs: 'course'
+      })
+      .when('/login_register',{
+        templateUrl:'app/main/login_register/login_register.html',
+        controller:'LoginRegisterCtrl',
+        controllerAs: 'loginRegister'
+      })
+      .when('/learning_information',{
+        templateUrl:'app/main/learning_information/learning_information.html',
+        controller:'Learning_informationCtrl',
+        controllerAs: 'learning_information'
+      })
+      .when('/enterprise_training',{
+        templateUrl:'app/main/enterprise_training/enterprise_training.html',
+        controller:'Enterprise_trainingCtrl',
+        controllerAs: 'enterprise_training'
+      })
+      .when('/contact_us',{
+        templateUrl:'app/main/contact_us/contact_us.html',
+        controller:'Contact_usCtrl',
+        controllerAs: 'contact_us'
+      })
       .otherwise({
         redirectTo: '/'
       });
