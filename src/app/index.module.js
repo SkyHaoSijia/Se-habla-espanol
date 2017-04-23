8c590d58928f2f73ca5f254297424fab7992be9e
陈庆锋 <maoqing1233@126.com>
2017-04-23 10:38:33 +0800

    init

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('seHablaEspanol', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngRoute', 'mgcrea.ngStrap', 'toastr']);
+
+})();
