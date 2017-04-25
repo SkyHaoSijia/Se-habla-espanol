394df87a1dd1365cd7c9eecf99f193192882f1be
陈庆锋 <maoqing1233@126.com>
2017-04-25 16:35:56 +0800

    add footer

---
+++ src/app/components/footer/footer.directive.js
@@ -0,0 +1,31 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('seHablaEspanol')
+    .directive('acmeFooter', acmeFooter);
+
+  /** @ngInject */
+  function acmeFooter() {
+    var directive = {
+      restrict: 'E',
+      templateUrl: 'app/components/footer/footer.html',
+      scope: {
+      },
+      controller: FooterController,
+      controllerAs: 'vm',
+      bindToController: true
+    };
+
+    return directive;
+
+    /** @ngInject */
+    function FooterController() {
+      var vm = this;
+      console.log(1);
+      // "vm.creationDate" is available by directive option "bindToController: true"
+      //vm.relativeDate = moment(vm.creationDate).fromNow();
+    }
+  }
+
+})();
